fd66839a824cc92e4015662a05a026920577f598
Siahei Audzeichyk <siarhei.audzeichyk@gmail.com>
2023-01-18 19:33:43 +0100

    Create initial models and migrate database

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -12,5 +13,9 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    from notes.models import db
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     return app
